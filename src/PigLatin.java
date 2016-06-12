bb7ff1958411e36e97e8171af26fc30e18988fd8
Ethan Joseph <EthanJoseph@Macintosh.local>
2016-06-12 19:49:16 -0400

    Remove debug print lines and changed println so results are all on the same line

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -38,77 +38,58 @@ public class PigLatin {
 		// start the loop here
 
 		do {
-			//write if else so that if the indexOf the first space returns anything less than zero
-			//the substring runs according to the length of the original
-			
+			// write if else so that if the indexOf the first space returns
+			// anything less than zero
+			// the substring runs according to the length of the original
+
 			if (original.indexOf(" ", startingPoint) >= 0) {
 				index = original.indexOf(" ", startingPoint);
-				System.out.println("if");
-			}
-			else {
-				
+			} else {
+
 				index = original.length();
-				System.out.println("else");
+
 			}
-				
-				// takes a substring from the startingPoint(0, but set to the
-				// new index after the loop to that index
-				// need to change 0 to be need to create another variable for
-				// the zero, because we need index at this point to be the new
-				// number
 
-				System.out.println("the index at this point is: " + index);
-				word = original.substring(startingPoint, index);
+			// takes a substring from the startingPoint(0, but set to the
+			// new index after the loop to that index
+			// need to change 0 to be need to create another variable for
+			// the zero, because we need index at this point to be the new
+			// number
 
-				System.out.println(word);
+			word = original.substring(startingPoint, index);
 
-				// if the word starts with a vowel, add "way" to the ending
-				// tells us the character at the starting point for the word
-				char letter1 = word.charAt(0);
+			// if the word starts with a vowel, add "way" to the ending
+			// tells us the character at the starting point for the word
+			char letter1 = word.charAt(0);
 
-				System.out.println(letter1);
+			// if that first letter is a vowel print the word with 'way' on
+			// the end
+			if (letter1 == 'a' || letter1 == 'e' || letter1 == 'i' || letter1 == 'o' || letter1 == 'u') {
+				System.out.print(word + "way ");
 
-				// if that first letter is a vowel print the word with 'way' on
-				// the end
-				if (letter1 == 'a' || letter1 == 'e' || letter1 == 'i' || letter1 == 'o' || letter1 == 'u') {
-					System.out.println(word + "way");
+				startingPoint = index + 1;
+			} // if it starts with a consonant, convert to pig latin for
+				// consonants
+			else {
+				// find the first vowel in this word
 
-					startingPoint = index + 1;
-				} // if it starts with a consonant, convert to pig latin for
-					// consonants
-				else {
-					// find the first vowel in this word
+				for (iVowel = 0; iVowel <= word.length(); iVowel++) {
+					if (word.charAt(iVowel) == 'a' || word.charAt(iVowel) == 'e' || word.charAt(iVowel) == 'i'
+							|| word.charAt(iVowel) == 'o' || word.charAt(iVowel) == 'u' || word.charAt(iVowel) == 'y') {
+						vowel = word.charAt(iVowel);
 
-					for (iVowel = 0; iVowel <= word.length(); iVowel++) {
-						if (word.charAt(iVowel) == 'a' || word.charAt(iVowel) == 'e' || word.charAt(iVowel) == 'i'
-								|| word.charAt(iVowel) == 'o' || word.charAt(iVowel) == 'u'
-								|| word.charAt(iVowel) == 'y') {
-							vowel = word.charAt(iVowel);
-							System.out.println("The first vowel in this word is: " + vowel); // this
-																								// is
-																								// the
-																								// char
-							System.out.println("the index of this vowel is: " + iVowel); // this
-																							// is
-																							// the
-																							// index
-																							// of
-																							// that
-																							// char
-							break; // might be able to avoid using a break if I
-									// use a while and set the condition to
-									// while (!== 'a', etc.)
-						}
-
+						break; // might be able to avoid using a break if I
+								// use a while and set the condition to
+								// while (!== 'a', etc.)
 					}
-					System.out.println("The length of this word is: " + word.length());
-					word = word.substring(iVowel, word.length());
-					System.out.println("pig latin result is: " + word + letter1 + "ay");
 
-					startingPoint = index + 1;
-					System.out.println("the startingPoint index is: " + startingPoint);
 				}
-		
-	} while (startingPoint <= (original.length() - word.length()));
+				word = word.substring(iVowel, word.length());
+				System.out.print(word + letter1 + "ay ");
+
+				startingPoint = index + 1;
+			}
+
+		} while (startingPoint <= (original.length() - word.length()));
 	}
 }
